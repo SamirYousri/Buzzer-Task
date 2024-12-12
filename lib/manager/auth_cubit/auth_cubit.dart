911e8051163d7d8abe1ec5cb4ec568e61e45db25
samirYousri <samiryousri96@gmail.com>
2024-12-12 13:01:35 +0200

    FirebaseAuthException done

--- lib/manager/auth_cubit/auth_cubit.dart
+++ lib/manager/auth_cubit/auth_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:auth_task/manager/auth_cubit/auth_state.dart';
 import 'package:auth_task/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthCubit extends Cubit<AuthState> {
@@ -13,8 +14,14 @@ class AuthCubit extends Cubit<AuthState> {
     try {
       await authRepository.login(email, password);
       emit(AuthSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        emit(AuthError('No user found for that email.'));
+      } else if (e.code == 'wrong-password') {
+        emit(AuthError('Wrong password provided for that user.'));
+      }
     } catch (e) {
-      emit(AuthError(e.toString()));
+      emit(AuthError('Something went wrong'));
     }
   }
 
@@ -23,8 +30,14 @@ class AuthCubit extends Cubit<AuthState> {
     try {
       await authRepository.register(email, password);
       emit(AuthSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        emit(AuthError('The password provided is too weak.'));
+      } else if (e.code == 'email-already-in-use') {
+        emit(AuthError('The account already exists for that email.'));
+      }
     } catch (e) {
-      emit(AuthError(e.toString()));
+      emit(AuthError('Something went wrong'));
     }
   }
 
